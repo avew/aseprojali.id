6e21838df41a1328222670fa2111d5d8cb20d75b
Asep Rojali <aseprojali@gmail.com>
2017-11-02 23:40:48 +0700

    add cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "aseprojali.id"
+set :repo_url, "git@github.com:avew/aseprojali.id.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/usr/share/nginx/blog"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -36,4 +36,4 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+set :ssh_options,{ :forward_agent => true }
